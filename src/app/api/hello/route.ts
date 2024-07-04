d0ced5e032469d3f500fb69ab7c69c735fb7321b
Schubert Rodriguez Quezada <schubert.quezada@galp.com>
2024-07-04 10:24:32 +0100

    chore: Add API route for hello endpoint

---
+++ src/app/api/hello/route.ts
@@ -0,0 +1,9 @@
+import { NextResponse, NextRequest } from "next/server";
+
+export async function GET(request: Request) {
+  return NextResponse.json({ message: "Hello World", method: "GET" });
+}
+
+export async function POST(request: Request) {
+  return NextResponse.json({ message: "Hello World", method: "POST" });
+}
