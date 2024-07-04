3c9d0698cc254be97156bfb11b0b66a747072cf4
Schubert Rodriguez Quezada <schubert.quezada@galp.com>
2024-07-04 12:34:50 +0100

    chore: Refactor GET route to support pagination

--- src/app/api/todos/route.ts
+++ src/app/api/todos/route.ts
@@ -2,7 +2,28 @@ import prisma from "@/lib/prisma";
 import { NextResponse, NextRequest } from "next/server";
 
 export async function GET(request: Request) {
-  const todos = await prisma.todo.findMany(); // Fetch all todos
+  const { searchParams } = new URL(request.url);
+  const take = Number(searchParams.get("take") ?? "10");
+  const skip = Number(searchParams.get("skip") ?? "0");
+
+  if (isNaN(take)) {
+    return NextResponse.json(
+      { error: "Invalid take parameter is not a number" },
+      { status: 400 }
+    );
+  }
+
+  if (isNaN(skip)) {
+    return NextResponse.json(
+      { error: "Invalid skip parameter is not a number" },
+      { status: 400 }
+    );
+  }
+
+  const todos = await prisma.todo.findMany({
+    take: take,
+    skip: skip,
+  }); // Fetch all todos
 
   return NextResponse.json(todos);
 }
