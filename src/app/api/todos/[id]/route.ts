589118dc14219c960d890c83c1900e319af236f8
Schubert Rodriguez Quezada <schubert.quezada@galp.com>
2024-07-08 10:22:39 +0100

    feat: Refactor PUT route to improve code readability and maintainability

--- src/app/api/todos/[id]/route.ts
+++ src/app/api/todos/[id]/route.ts
@@ -1,5 +1,7 @@
 import prisma from "@/lib/prisma";
+import { Todo } from "@prisma/client";
 import { NextResponse, NextRequest } from "next/server";
+import * as yup from "yup";
 
 interface Segments {
   params: {
@@ -7,14 +9,43 @@ interface Segments {
   };
 }
 
-export async function GET(request: Request, { params }: Segments) {
-  const { id } = params;
+const getTodo = async (id: string): Promise<Todo | null> => {
   const todo = await prisma.todo.findFirst({ where: { id } });
+  return todo;
+};
+
+export async function GET(request: Request, { params }: Segments) {
+  const todo = await getTodo(params.id);
   if (!todo) {
-    return NextResponse.json(
-      { error: "Todo not found" },
-      { status: 404 }
-    );
+    return NextResponse.json({ error: "Todo not found" }, { status: 404 });
   }
   return NextResponse.json({ todo });
+}
+
+const putSchema = yup.object({
+  description: yup.string().optional(),
+  completed: yup.boolean().optional(),
+});
+
+export async function PUT(req: Request, { params }: Segments) {
+  const { id } = params;
+  const todo = await getTodo(id);
+  if (!todo) {
+    return NextResponse.json({ error: "Todo not found" }, { status: 404 });
+  }
+
+  try {
+    const { completed, description } = await putSchema.validate(
+      await req.json()
+    );
+
+    const updatedTodo = await prisma.todo.update({
+      where: { id },
+      data: { completed, description },
+    });
+
+    return NextResponse.json({ updatedTodo });
+  } catch (error) {
+    return NextResponse.json(error, { status: 400 });
+  }
 }
