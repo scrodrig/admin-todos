29c2caab4180f3391db08fe76637227b7d799610
Schubert Rodriguez Quezada <schubert.quezada@galp.com>
2024-07-08 16:13:56 +0100

    chore: Update DashboardPage to use WidgetItem component in a grid layout

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,9 @@
+import { WidgetItem } from "@/components";
+
 export default function DashboardPage() {
   return (
-    <div>
-      <h1>Hello dashboard</h1>
+    <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
+      <WidgetItem />
     </div>
   );
 }
