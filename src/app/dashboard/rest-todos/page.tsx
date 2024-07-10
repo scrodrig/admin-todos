fa25691fe14dbd78a207f42f3bb864594a5229e5
Schubert Rodriguez Quezada <schubert.quezada@galp.com>
2024-07-10 14:53:17 +0100

    refactor: Update RestTodosPage to fetch and display todos from the database

--- src/app/dashboard/rest-todos/page.tsx
+++ src/app/dashboard/rest-todos/page.tsx
@@ -1,8 +1,21 @@
+import prisma from "@/lib/prisma";
 
-export default function RestTodosPage() {
+export const metadata = {
+  title: "Rest todos",
+  description: "Rest todos page",
+};
+
+export default async function RestTodosPage() {
+  const todos = await prisma.todo.findMany({
+    orderBy: {
+      description: "asc",
+    },
+  });
+
   return (
     <div>
       <h1>Hello Rest todos</h1>
+      <p>{JSON.stringify(todos)}</p>
     </div>
   );
 }
