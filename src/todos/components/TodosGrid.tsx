06c4101d595c6664e6f0829d8915bee026713574
Schubert Rodriguez Quezada <schubert.quezada@galp.com>
2024-07-11 10:16:24 +0100

    refactor: Update TodosGrid component to use lazy loading for images

--- src/todos/components/TodosGrid.tsx
+++ src/todos/components/TodosGrid.tsx
@@ -1,17 +1,20 @@
-import { Todo } from "@prisma/client";
-import React from "react";
-import { TodoItem } from "./TodoItem";
+'use client'
+
+import { Todo } from '@prisma/client'
+import React from 'react'
+import { TodoItem } from './TodoItem'
+import * as api from '@/todos/helper/todos'
 
 interface TodosGridProps {
-  todos?: Todo[];
+  todos?: Todo[]
 }
 
 export const TodosGrid = ({ todos = [] }: TodosGridProps) => {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-3 gap-2">
       {todos.map((todo) => (
-        <TodoItem key={todo.id} todo={todo} />
+        <TodoItem key={todo.id} todo={todo} toggleTodo={api.updateTodo} />
       ))}
     </div>
-  );
-};
+  )
+}
