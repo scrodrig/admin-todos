104efd99e4cceb720c1b06b318aa0f6cd7663624
Schubert Rodriguez Quezada <schubert.quezada@galp.com>
2024-07-11 10:16:21 +0100

    refactor: Update TodoItem component to toggle todo completion on click

--- src/todos/components/TodoItem.tsx
+++ src/todos/components/TodoItem.tsx
@@ -1,22 +1,25 @@
-import React from "react";
-import { Todo } from "@prisma/client";
+'use client'
 
-import styles from "./TodoItem.module.css";
-import { IoCheckboxOutline, IoSquareOutline } from "react-icons/io5";
+import React from 'react'
+import { Todo } from '@prisma/client'
+
+import styles from './TodoItem.module.css'
+import { IoCheckboxOutline, IoSquareOutline } from 'react-icons/io5'
 
 interface TodoItemProps {
-  todo: Todo;
+  todo: Todo
+  toggleTodo: (id: string, completed: boolean) => Promise<Todo | void>
 }
 
-export const TodoItem = ({ todo }: TodoItemProps) => {
+export const TodoItem = ({ todo, toggleTodo }: TodoItemProps) => {
   return (
     <div className={todo.completed ? styles.todoDone : styles.todoPending}>
       <div className="flex flex-col sm:flex-row justify-start items-center gap-4">
         <div
+          onClick={() => toggleTodo(todo.id, !todo.completed)}
           className={`flex p-2 rounded-me cursor-pointer hover:bg-opacity-60 ${
-            todo.completed ? "bg-blue-100" : "bg-red-100"
-          }`}
-        >
+            todo.completed ? 'bg-blue-100' : 'bg-red-100'
+          }`}>
           {todo.completed ? (
             <IoCheckboxOutline className="text-2xl text-green-500" />
           ) : (
@@ -26,5 +29,5 @@ export const TodoItem = ({ todo }: TodoItemProps) => {
         <div className="text-center sm:text-left">{todo.description}</div>
       </div>
     </div>
-  );
-};
+  )
+}
