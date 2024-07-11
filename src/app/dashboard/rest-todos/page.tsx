0622af0958d80d47f64347f68a5bad1d17575a03
Schubert Rodriguez Quezada <schubert.quezada@galp.com>
2024-07-11 11:44:17 +0100

    refactor: Update RestTodosPage component for lazy loading and styling improvements

--- src/app/dashboard/rest-todos/page.tsx
+++ src/app/dashboard/rest-todos/page.tsx
@@ -1,24 +1,26 @@
-import prisma from "@/lib/prisma";
-import { TodosGrid } from "@/todos";
+import prisma from '@/lib/prisma'
+import { NewTodo, TodosGrid } from '@/todos'
 
 export const metadata = {
-  title: "Rest todos",
-  description: "Rest todos page",
-};
+  title: 'Rest todos',
+  description: 'Rest todos page',
+}
 
 export default async function RestTodosPage() {
   const todos = await prisma.todo.findMany({
     orderBy: {
-      description: "asc",
+      description: 'asc',
     },
-  });
+  })
 
   //?INFO: useState and useEffect force client side rendering
 
   return (
     <div>
-      <h1>Hello Rest todos</h1>
+      <div className="w-full ml-5 mr-5 px-3 mb-5">
+        <NewTodo />
+      </div>
       <TodosGrid todos={todos} />
     </div>
-  );
+  )
 }
