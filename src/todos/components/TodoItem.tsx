6d6902d2cd4ae6cac82048753275b4a3f28c82ac
Schubert Rodriguez Quezada <schubert.quezada@galp.com>
2024-07-11 15:06:37 +0100

    refactor: Optimize TodoItem component with useOptimistic hook for better performance

--- src/todos/components/TodoItem.tsx
+++ src/todos/components/TodoItem.tsx
@@ -1,7 +1,8 @@
 'use client'
 
-import React from 'react'
+import React, { startTransition } from 'react'
 import { Todo } from '@prisma/client'
+import { useOptimistic } from 'react'
 
 import styles from './TodoItem.module.css'
 import { IoCheckboxOutline, IoSquareOutline } from 'react-icons/io5'
@@ -12,21 +13,39 @@ interface TodoItemProps {
 }
 
 export const TodoItem = ({ todo, toggleTodo }: TodoItemProps) => {
+  const [todoOptimistic, toggleTodoOptimistic] = useOptimistic(
+    todo,
+    (state, newCompletedValue: boolean) => ({
+      ...state,
+      completed: newCompletedValue,
+    })
+  )
+
+  const onToggleTodo = async () => {
+    try {
+      startTransition(() => toggleTodoOptimistic(!todoOptimistic.completed))
+      await toggleTodo(todoOptimistic.id, !todoOptimistic.completed)
+    } catch (error) {
+      startTransition(() => toggleTodoOptimistic(!todoOptimistic.completed))
+    }
+  }
+
   return (
-    <div className={todo.completed ? styles.todoDone : styles.todoPending}>
+    <div className={todoOptimistic.completed ? styles.todoDone : styles.todoPending}>
       <div className="flex flex-col sm:flex-row justify-start items-center gap-4">
         <div
-          onClick={() => toggleTodo(todo.id, !todo.completed)}
+          // onClick={() => toggleTodo(todoOptimistic.id, !todoOptimistic.completed)}
+          onClick={() => onToggleTodo()}
           className={`flex p-2 rounded-me cursor-pointer hover:bg-opacity-60 ${
-            todo.completed ? 'bg-blue-100' : 'bg-red-100'
+            todoOptimistic.completed ? 'bg-blue-100' : 'bg-red-100'
           }`}>
-          {todo.completed ? (
+          {todoOptimistic.completed ? (
             <IoCheckboxOutline className="text-2xl text-green-500" />
           ) : (
             <IoSquareOutline className="text-2xl text-gray-500" />
           )}
         </div>
-        <div className="text-center sm:text-left">{todo.description}</div>
+        <div className="text-center sm:text-left">{todoOptimistic.description}</div>
       </div>
     </div>
   )
