d088c6c6f6152e406439b5e4f15cae49b610d773
Schubert Rodriguez Quezada <schubert.quezada@galp.com>
2024-07-11 10:00:21 +0100

    refactor: Update TodoItem component to use CSS modules for styling

--- src/todos/components/TodoItem.tsx
+++ src/todos/components/TodoItem.tsx
@@ -1,10 +1,30 @@
 import React from "react";
 import { Todo } from "@prisma/client";
 
+import styles from "./TodoItem.module.css";
+import { IoCheckboxOutline, IoSquareOutline } from "react-icons/io5";
+
 interface TodoItemProps {
   todo: Todo;
 }
 
 export const TodoItem = ({ todo }: TodoItemProps) => {
-  return <div>{todo.description}</div>;
+  return (
+    <div className={todo.completed ? styles.todoDone : styles.todoPending}>
+      <div className="flex flex-col sm:flex-row justify-start items-center gap-4">
+        <div
+          className={`flex p-2 rounded-me cursor-pointer hover:bg-opacity-60 ${
+            todo.completed ? "bg-blue-100" : "bg-red-100"
+          }`}
+        >
+          {todo.completed ? (
+            <IoCheckboxOutline className="text-2xl text-green-500" />
+          ) : (
+            <IoSquareOutline className="text-2xl text-gray-500" />
+          )}
+        </div>
+        <div className="text-center sm:text-left">{todo.description}</div>
+      </div>
+    </div>
+  );
 };
