5d6ae98e9044b24444d7f47de458b3c778935a5d
Schubert Rodriguez Quezada <schubert.quezada@galp.com>
2024-07-11 14:23:14 +0100

    refactor: Update ServerTodosPage component to include server-side rendering and improve styling

---
+++ src/app/dashboard/server-todos/page.tsx
@@ -0,0 +1,27 @@
+import prisma from '@/lib/prisma'
+import { NewTodo, TodosGrid } from '@/todos'
+
+export const metadata = {
+  title: 'Server todos',
+  description: 'Server todos page',
+}
+
+export default async function ServerTodosPage() {
+  const todos = await prisma.todo.findMany({
+    orderBy: {
+      description: 'asc',
+    },
+  })
+
+  //?INFO: useState and useEffect force client side rendering
+
+  return (
+    <>
+      <span className="text-3xl mb-10">Server Actions</span>
+      <div className="w-full ml-5 mr-5 px-3 mb-5">
+        <NewTodo />
+      </div>
+      <TodosGrid todos={todos} />
+    </>
+  )
+}
