d3e5a835b060eabd706bd5b420873c2c2273d8d8
Schubert Rodriguez Quezada <schubert.quezada@galp.com>
2024-07-18 16:21:26 +0100

    refactor: Update CookiesPage for cookie server

--- src/app/dashboard/cookies/page.tsx
+++ src/app/dashboard/cookies/page.tsx
@@ -1,4 +1,5 @@
 import { TabBar } from '@/components'
+import { cookies } from 'next/headers'
 
 export const metadata = {
   title: 'Cookies Page',
@@ -6,11 +7,14 @@ export const metadata = {
 }
 
 export default function CookiesPage() {
+  const cookieStore = cookies()
+  const cookieTab = Number(cookieStore.get('selectedTab')?.value ?? '1')
+
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 gap-3">
       <div className="flex flex-col">
         <span className="text-3xl">Tabs</span>
-        <TabBar />
+        <TabBar currentTab={cookieTab} />
       </div>
     </div>
   )
