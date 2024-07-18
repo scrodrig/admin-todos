c0a567476f8e1ecc0073c201f747f11dc1d84e76
Schubert Rodriguez Quezada <schubert.quezada@galp.com>
2024-07-18 14:46:46 +0100

    refactor: Update DashboardLayout to include a bottom padding

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -12,7 +12,7 @@ export default function DashboardLayout({
       <Sidebar />
       <div className="ml-auto mb-6 lg:w-[75%] xl:w-[80%] 2xl:w-[85%] min-h-screen">
         <TopMenu />
-        <div className="px-6 pt-6 bg-white p-3 m-2 rounded">{children}</div>
+        <div className="px-6 pt-6 bg-white p-3 m-2 pb-5 rounded">{children}</div>
       </div>
     </>
   );
