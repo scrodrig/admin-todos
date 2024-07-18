878d0a865f8b31d714dcab04a903acb0ed9f098a
Schubert Rodriguez Quezada <schubert.quezada@galp.com>
2024-07-18 17:17:28 +0100

    refactor: Update ProductCard component to include removeProductFromCart action

--- src/products/components/ProductCard.tsx
+++ src/products/components/ProductCard.tsx
@@ -4,7 +4,7 @@
 import Image from 'next/image'
 import { IoAddCircleOutline, IoTrashOutline } from 'react-icons/io5'
 import { Star } from './Star'
-import { addProductToCart } from '@/shopping-cart/actions/actions'
+import { addProductToCart, removeProductFromCart } from '@/shopping-cart/actions/actions'
 import { useRouter } from 'next/navigation'
 
 interface Props {
@@ -16,11 +16,15 @@ interface Props {
 }
 
 export const ProductCard = ({ id, name, price, rating, image }: Props) => {
-
   const router = useRouter()
 
   const onAddToCart = () => {
     addProductToCart(id)
+    router.refresh()
+  }
+
+  const onRemoveFromCart = () => {
+    removeProductFromCart(id)
     router.refresh()
   }
 
@@ -64,7 +68,9 @@ export const ProductCard = ({ id, name, price, rating, image }: Props) => {
               className="text-white mr-2 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
               <IoAddCircleOutline size={25} />
             </button>
-            <button className="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">
+            <button
+              onClick={onRemoveFromCart}
+              className="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">
               <IoTrashOutline size={20} />
             </button>
           </div>
