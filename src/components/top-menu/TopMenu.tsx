8d599270c584f69b89cef90b572b8e09848a6e47
Schubert Rodriguez Quezada <schubert.quezada@galp.com>
2024-07-18 17:11:10 +0100

    feat: Update TopMenu component with lazy loading for images and shopping cart count

--- src/components/top-menu/TopMenu.tsx
+++ src/components/top-menu/TopMenu.tsx
@@ -1,6 +1,21 @@
-import { CiBellOn, CiChat1, CiMenuBurger, CiSearch, CiShoppingBasket } from "react-icons/ci";
+import { cookies } from 'next/headers'
+import { useRouter } from 'next/navigation'
+import { CiChat1, CiMenuBurger, CiSearch, CiShoppingBasket } from 'react-icons/ci'
 
 export const TopMenu = () => {
+  const cookieStore = cookies()
+  const cart = JSON.parse(cookieStore.get('cart')?.value ?? '{}')
+
+  const getTotalCount = () => {
+    let items = 0
+
+    Object.values(cart).forEach((count) => {
+      items += count as number
+    })
+
+    return items
+  }
+
   return (
     <div className="sticky z-10 top-0 h-16 border-b bg-white lg:py-2.5">
       <div className="px-6 flex items-center justify-between space-x-4">
@@ -33,11 +48,11 @@ export const TopMenu = () => {
             <CiChat1 size={25} />
           </button>
           <button className="p-2 flex items-center justify-center h-10 rounded-xl border bg-gray-100 focus:bg-gray-100 active:bg-gray-200">
-            <span className="text-sm mr-2 text-blue-800">10</span>
+            <span className="text-sm mr-2 text-blue-800">{getTotalCount()}</span>
             <CiShoppingBasket size={25} />
           </button>
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
