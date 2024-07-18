f086d9d1efb03625f832bc11acaea3dd574479a9
Schubert Rodriguez Quezada <schubert.quezada@galp.com>
2024-07-18 17:05:08 +0100

    feat: Update TopMenu component with shopping cart icon and count

--- src/components/top-menu/TopMenu.tsx
+++ src/components/top-menu/TopMenu.tsx
@@ -1,4 +1,4 @@
-import { CiBellOn, CiChat1, CiMenuBurger, CiSearch } from "react-icons/ci";
+import { CiBellOn, CiChat1, CiMenuBurger, CiSearch, CiShoppingBasket } from "react-icons/ci";
 
 export const TopMenu = () => {
   return (
@@ -32,8 +32,9 @@ export const TopMenu = () => {
           <button className="flex items-center justify-center w-10 h-10 rounded-xl border bg-gray-100 focus:bg-gray-100 active:bg-gray-200">
             <CiChat1 size={25} />
           </button>
-          <button className="flex items-center justify-center w-10 h-10 rounded-xl border bg-gray-100 focus:bg-gray-100 active:bg-gray-200">
-            <CiBellOn size={25} />
+          <button className="p-2 flex items-center justify-center h-10 rounded-xl border bg-gray-100 focus:bg-gray-100 active:bg-gray-200">
+            <span className="text-sm mr-2 text-blue-800">10</span>
+            <CiShoppingBasket size={25} />
           </button>
         </div>
       </div>
