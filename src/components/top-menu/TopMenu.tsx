9de2c8f7c71a3f208f12b90fd8c515283efeb3cb
Schubert Rodriguez Quezada <schubert.quezada@galp.com>
2024-07-18 17:17:33 +0100

    refactor: Optimize TopMenu component for better performance

--- src/components/top-menu/TopMenu.tsx
+++ src/components/top-menu/TopMenu.tsx
@@ -1,20 +1,22 @@
+import { get } from 'http'
 import { cookies } from 'next/headers'
 import { useRouter } from 'next/navigation'
 import { CiChat1, CiMenuBurger, CiSearch, CiShoppingBasket } from 'react-icons/ci'
 
+const getTotalCount = (cart: { [id: string]: number }): number => {
+  let items = 0
+
+  Object.values(cart).forEach((count) => {
+    items += count as number
+  })
+
+  return items
+}
+
 export const TopMenu = () => {
   const cookieStore = cookies()
   const cart = JSON.parse(cookieStore.get('cart')?.value ?? '{}')
-
-  const getTotalCount = () => {
-    let items = 0
-
-    Object.values(cart).forEach((count) => {
-      items += count as number
-    })
-
-    return items
-  }
+  const totalItems = getTotalCount(cart)
 
   return (
     <div className="sticky z-10 top-0 h-16 border-b bg-white lg:py-2.5">
@@ -48,7 +50,7 @@ export const TopMenu = () => {
             <CiChat1 size={25} />
           </button>
           <button className="p-2 flex items-center justify-center h-10 rounded-xl border bg-gray-100 focus:bg-gray-100 active:bg-gray-200">
-            <span className="text-sm mr-2 text-blue-800">{getTotalCount()}</span>
+            {totalItems > 0 && <span className="text-sm mr-2 text-blue-800">{totalItems}</span>}
             <CiShoppingBasket size={25} />
           </button>
         </div>
