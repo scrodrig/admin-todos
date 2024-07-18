d3d209c9ba7a5966059155ea9989e7ca4ed61bb6
Schubert Rodriguez Quezada <schubert.quezada@galp.com>
2024-07-18 16:31:39 +0100

    feat: Create ProductsPage component with ProductCard

---
+++ src/app/dashboard/products/page.tsx
@@ -0,0 +1,9 @@
+import { ProductCard } from "@/products";
+
+export default function ProductsPage() {
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-3 gap-3">
+      <ProductCard />
+    </div>
+  );
+}
