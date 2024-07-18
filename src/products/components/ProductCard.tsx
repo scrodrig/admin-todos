9e8613de5e07b39b9cfe4744aaab8df3f7256343
Schubert Rodriguez Quezada <schubert.quezada@galp.com>
2024-07-18 17:05:01 +0100

    feat: Add onAddToCart function to ProductCard component

--- src/products/components/ProductCard.tsx
+++ src/products/components/ProductCard.tsx
@@ -1,8 +1,10 @@
+'use client'
 // https://tailwindcomponents.com/component/e-commerce-product-card
 
 import Image from 'next/image'
 import { IoAddCircleOutline, IoTrashOutline } from 'react-icons/io5'
 import { Star } from './Star'
+import { addProductToCart } from '@/shopping-cart/actions/actions'
 
 interface Props {
   id: string
@@ -13,6 +15,10 @@ interface Props {
 }
 
 export const ProductCard = ({ id, name, price, rating, image }: Props) => {
+  const onAddToCart = () => {
+    addProductToCart(id)
+  }
+
   return (
     <div className="bg-white shadow rounded-lg max-w-sm dark:bg-gray-800 dark:border-gray-100">
       {/* Product Image */}
@@ -48,7 +54,9 @@ export const ProductCard = ({ id, name, price, rating, image }: Props) => {
           </span>
 
           <div className="flex">
-            <button className="text-white mr-2 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+            <button
+              onClick={onAddToCart}
+              className="text-white mr-2 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
               <IoAddCircleOutline size={25} />
             </button>
             <button className="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">
