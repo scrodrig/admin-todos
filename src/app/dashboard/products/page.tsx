5277cdeb1824b20c9f0d8fe65484dcbf487dd5dc
Schubert Rodriguez Quezada <schubert.quezada@galp.com>
2024-07-18 16:42:40 +0100

    feat: Update ProductsPage to render ProductCard with lazy loaded images

--- src/app/dashboard/products/page.tsx
+++ src/app/dashboard/products/page.tsx
@@ -1,9 +1,12 @@
-import { ProductCard } from "@/products";
+import { ProductCard } from '@/products'
+import { products } from '@/products/data/products'
 
 export default function ProductsPage() {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-3 gap-3">
-      <ProductCard />
+      {products.map((product) => {
+        return <ProductCard key={product.id} {...product} />
+      })}
     </div>
-  );
+  )
 }
