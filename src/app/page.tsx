128df57883dc2f3154b638d469dd7590bb459d66
Schubert Rodriguez Quezada <schubert.quezada@galp.com>
2024-07-19 16:12:20 +0100

    refactor: Update Home page layout and remove unnecessary code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <>
+    <div className="grid gap-6">
       <span className="text-5xl">Hello world</span>
-    </>
+    </div>
   );
 }
