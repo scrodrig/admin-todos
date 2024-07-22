7dc944dbfb869c7355cfa24fc82cf2582123b113
Schubert Rodriguez Quezada <schubert.quezada@galp.com>
2024-07-22 16:00:19 +0100

    feat: Add Avatar component

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import React from "react";
 import { CiBookmarkCheck, CiLogout, CiShoppingBasket, CiViewList } from "react-icons/ci";
 import SidebarItem from "./SidebarItem";
+import { Avatar } from "../avatar/Avatar";
 
 const menuItems = [
   {
@@ -48,19 +49,7 @@ export const Sidebar = () => {
           </Link>
         </div>
 
-        <div className="mt-8 text-center">
-          <Image
-            src="https://tailus.io/sources/blocks/stats-cards/preview/images/second_user.webp"
-            alt=""
-            width={100}
-            height={100}
-            className="w-10 h-10 m-auto rounded-full object-cover lg:w-28 lg:h-28"
-          />
-          <h5 className="hidden mt-4 text-xl font-semibold text-gray-600 lg:block">
-            @scrodrig
-          </h5>
-          <span className="hidden text-gray-400 lg:block">Admin</span>
-        </div>
+        <Avatar />
 
         <ul className="space-y-2 tracking-wide mt-8">
           {menuItems.map((item) => (
