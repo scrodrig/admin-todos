a2617b044294713f866a616ea2b594b3d9cbfe26
Schubert Rodriguez Quezada <schubert.quezada@galp.com>
2024-07-22 16:00:16 +0100

    feat: Add Avatar component

---
+++ src/components/avatar/Avatar.tsx
@@ -0,0 +1,27 @@
+import Image from 'next/image'
+import { getServerSession } from 'next-auth'
+import { redirect } from 'next/navigation'
+
+export const Avatar = async () => {
+  const session = await getServerSession()
+
+  if (!session) {
+    redirect('/api/auth/signin')
+  }
+
+  const { user } = session
+
+  return (
+    <div className="mt-8 text-center">
+      <Image
+        src={user?.image ?? 'https://tailus.io/sources/blocks/stats-cards/preview/images/logo.svg'}
+        alt=""
+        width={100}
+        height={100}
+        className="w-10 h-10 m-auto rounded-full object-cover lg:w-28 lg:h-28"
+      />
+      <h5 className="hidden mt-4 text-xl font-semibold text-gray-600 lg:block">{user?.name}</h5>
+      <span className="hidden text-gray-400 lg:block">Admin</span>
+    </div>
+  )
+}
