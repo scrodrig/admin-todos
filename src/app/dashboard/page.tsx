8a6bf0cfa9a45c637aa31c40a26ddaf22a036875
Schubert Rodriguez Quezada <schubert.quezada@galp.com>
2024-07-22 16:00:10 +0100

    refactor: Fix typo in DashboardPage grid class name

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -10,7 +10,7 @@ export default async function DashboardPage() {
   }
 
   return (
-    <div className="grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
+    <div className="grid gap-6 grid-cols-1 sm:griad-cols-2 lg:grid-cols-3">
       <WidgetItem title="Conected user server side">
         <div className='flex flex-col'>
           <span className="text-2xl">{session.user?.name}</span>
