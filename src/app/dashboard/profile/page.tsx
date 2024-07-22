c7f8a37fdd14046d675b7e82e361efc2281ea6a5
Schubert Rodriguez Quezada <schubert.quezada@galp.com>
2024-07-22 16:15:37 +0100

    feat: Add profile page component

---
+++ src/app/dashboard/profile/page.tsx
@@ -0,0 +1,24 @@
+'use client'
+
+import { useSession } from 'next-auth/react'
+import { useEffect } from 'react'
+
+export default function ProfilePage() {
+  const { data: session } = useSession()
+
+  useEffect(() => {
+    console.log('client side')
+  }, [])
+
+  return (
+    <div>
+      <h1>Profile Page</h1>
+      <hr />
+      <div className="flex flex-col">
+        <span>{session?.user?.name}</span>
+        <span>{session?.user?.email}</span>
+        <span>{session?.user?.image}</span>
+      </div>
+    </div>
+  )
+}
