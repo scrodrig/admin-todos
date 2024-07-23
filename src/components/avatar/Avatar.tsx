6ae45b58bda274b9656c4af088c202b8e7deba6a
Schubert Rodriguez Quezada <schubert.quezada@galp.com>
2024-07-23 16:00:45 +0100

    refactor: Update Avatar component to display user roles

--- src/components/avatar/Avatar.tsx
+++ src/components/avatar/Avatar.tsx
@@ -1,15 +1,18 @@
 import Image from 'next/image'
 import { getServerSession } from 'next-auth'
 import { redirect } from 'next/navigation'
+import { authOptions } from '@/app/api/auth/[...nextauth]/route'
 
 export const Avatar = async () => {
-  const session = await getServerSession()
+  const session = await getServerSession(authOptions)
 
   if (!session) {
     redirect('/api/auth/signin')
   }
 
   const { user } = session
+
+  const userRoles = user?.roles ?? ['client']
 
   return (
     <div className="mt-8 text-center">
@@ -21,7 +24,7 @@ export const Avatar = async () => {
         className="w-10 h-10 m-auto rounded-full object-cover lg:w-28 lg:h-28"
       />
       <h5 className="hidden mt-4 text-xl font-semibold text-gray-600 lg:block">{user?.name}</h5>
-      <span className="hidden text-gray-400 lg:block">Admin</span>
+      <span className="hidden text-gray-400 lg:block capitalize">{userRoles.join(',')}</span>
     </div>
   )
 }
