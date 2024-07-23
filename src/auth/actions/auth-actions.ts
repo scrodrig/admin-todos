7b8cfc36a0a2801f73d71ae524b749f1ff07d230
Schubert Rodriguez Quezada <schubert.quezada@galp.com>
2024-07-23 16:42:48 +0100

    refactor: Add sign-in functionality with email and password authentication

---
+++ src/auth/actions/auth-actions.ts
@@ -0,0 +1,33 @@
+import prisma from '@/lib/prisma'
+import bcrypt from 'bcryptjs'
+
+export const signInEmailPassword = async (email?: string, password?: string) => {
+  if (!email || !password) {
+    return null
+  }
+
+  const user = await prisma.user.findUnique({ where: { email: email } })
+
+  if (!user) {
+    const dbUser = await createUser(email, password)
+    return dbUser
+  }
+
+  if (!bcrypt.compareSync(password, user.password ?? '')) {
+    return null
+  }
+
+  return user
+}
+
+const createUser = async (email: string, password: string) => {
+  const user = await prisma.user.create({
+    data: {
+      email: email,
+      password: bcrypt.hashSync(password, 10),
+      name: email.split('@')[0],
+    },
+  })
+
+  return user
+}
