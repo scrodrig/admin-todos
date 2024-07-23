16c88cccdc5c17dae98e0c5105241aab959af289
Schubert Rodriguez Quezada <schubert.quezada@galp.com>
2024-07-23 12:40:55 +0100

    refactor: Redirect to '/dashboard' on Home page load

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import { redirect } from 'next/navigation'
+
 export default function Home() {
+  redirect('/dashboard')
   return (
     <div className="grid gap-6">
       <span className="text-5xl">Hello world</span>
     </div>
-  );
+  )
 }
