337fe0c79c0a3521d7510a6bddafa944e1fa6db3
Schubert Rodriguez Quezada <schubert.quezada@galp.com>
2024-07-23 16:58:45 +0100

    refactor: Seed database with test user and todos

--- src/app/api/seed/route.ts
+++ src/app/api/seed/route.ts
@@ -1,19 +1,29 @@
-import prisma from "@/lib/prisma";
-import { NextResponse, NextRequest } from "next/server";
+import prisma from '@/lib/prisma'
+import { NextResponse, NextRequest } from 'next/server'
+import bcrypt from 'bcryptjs'
 
 export async function GET(request: Request) {
-  await prisma.todo.deleteMany({}); // Delete all todos
+  await prisma.todo.deleteMany({}) // Delete all todos
+  await prisma.user.deleteMany({}) // Delete all users
 
-  await prisma.todo.createMany({
-    data: [
-      { description: "Space stone", completed: true },
-      { description: "Mind stone" },
-      { description: "Reality stone" },
-      { description: "Power stone" },
-      { description: "Time stone" },
-      { description: "Soul stone" },
-    ],
-  }); // Seed todos
+  const user = await prisma.user.create({
+    data: {
+      email: 'test1@usermail.com',
+      password: bcrypt.hashSync('123456', 10),
+      name: 'Test User',
+      roles: ['admin', 'user'],
+      todos: {
+        create: [
+          { description: 'Space stone', completed: true },
+          { description: 'Mind stone' },
+          { description: 'Reality stone' },
+          { description: 'Power stone' },
+          { description: 'Time stone' },
+          { description: 'Soul stone' },
+        ],
+      },
+    },
+  }) // Seed user
 
-  return NextResponse.json({ message: "Seed executed!" });
+  return NextResponse.json({ message: 'Seed executed!' })
 }
