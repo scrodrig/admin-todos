5d52b4f2cb262f50fdf50b2f242454ab341f9b75
Schubert Rodriguez Quezada <schubert.quezada@galp.com>
2024-07-23 17:23:39 +0100

    refactor: Add lazy loading for images in RestTodosPage

--- src/app/dashboard/rest-todos/page.tsx
+++ src/app/dashboard/rest-todos/page.tsx
@@ -1,9 +1,11 @@
 //! ERROR this is not working as expected
-export const dynamic = 'force-dynamic';
-export const revalidate = 0;
+export const dynamic = 'force-dynamic'
+export const revalidate = 0
 
+import { getUserServerSession } from '@/auth/actions/auth-actions'
 import prisma from '@/lib/prisma'
 import { NewTodo, TodosGrid } from '@/todos'
+import { redirect } from 'next/navigation'
 
 export const metadata = {
   title: 'Rest todos',
@@ -11,7 +13,14 @@ export const metadata = {
 }
 
 export default async function RestTodosPage() {
+  const user = await getUserServerSession()
+
+  if (!user) {
+    redirect('/api/auth/signin')
+  }
+
   const todos = await prisma.todo.findMany({
+    where: { userId: user.id },
     orderBy: {
       description: 'asc',
     },
