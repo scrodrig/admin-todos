ed996fec4c6d7a46d4e751280ae6a3f650396de6
Schubert Rodriguez Quezada <schubert.quezada@galp.com>
2024-07-23 16:42:44 +0100

    refactor: Add CredentialsProvider for email/password authentication

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -4,7 +4,8 @@ import NextAuth, { NextAuthOptions } from 'next-auth'
 import { Adapter } from 'next-auth/adapters'
 import GithubProvider from 'next-auth/providers/github'
 import GoogleProvider from 'next-auth/providers/google'
-import async from '../../../dashboard/page'
+import CredentialsProvider from 'next-auth/providers/credentials'
+import { signInEmailPassword } from '@/auth/actions/auth-actions'
 
 export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma) as Adapter,
@@ -17,6 +18,27 @@ export const authOptions: NextAuthOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_ID ?? '',
       clientSecret: process.env.GOOGLE_SECRET ?? '',
+    }),
+    CredentialsProvider({
+      // The name to display on the sign in form (e.g. "Sign in with...")
+      name: 'Credentials',
+      credentials: {
+        email: { label: 'Email', type: 'email', placeholder: 'user@email.com' },
+        password: { label: 'Password', type: 'password', placeholder: 'password' },
+      },
+
+      async authorize(credentials, req) {
+        // Add logic here to look up the user from the credentials supplied
+        const user = await signInEmailPassword(credentials?.email, credentials?.password)
+
+        if (user) {
+          // Any object returned will be saved in `user` property of the JWT
+          return user
+        }
+        // If you return null then an error will be displayed advising the user to check their details.
+        return null
+        // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
+      },
     }),
     // ...add more providers here
   ],
