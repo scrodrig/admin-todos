219f17aa6021ff8e4acdf31f3344f062dda3a08d
Schubert Rodriguez Quezada <schubert.quezada@galp.com>
2024-07-23 16:00:39 +0100

    refactor: Update ProfilePage component to display user information

--- src/app/dashboard/profile/page.tsx
+++ src/app/dashboard/profile/page.tsx
@@ -15,9 +15,11 @@ export default function ProfilePage() {
       <h1>Profile Page</h1>
       <hr />
       <div className="flex flex-col">
+        <span>{session?.user?.id}</span>
         <span>{session?.user?.name}</span>
         <span>{session?.user?.email}</span>
         <span>{session?.user?.image}</span>
+        <span>{session?.user?.roles?.join(',')}</span>
       </div>
     </div>
   )
