ff2f9815dcea10f90c8517d4e8d8df93a7441bed
Schubert Rodriguez Quezada <schubert.quezada@galp.com>
2024-07-24 17:47:58 +0100

    refactor: Update WidgetItem component to use bold font for title

--- src/components/widget-item/WidgetItem.tsx
+++ src/components/widget-item/WidgetItem.tsx
@@ -5,10 +5,10 @@ interface WidgetItemProps {
 
 export const WidgetItem = ({ children, title = 'Global Activities' }: WidgetItemProps) => {
   return (
-    <div className="md:col-span-2 lg:col-span-1">
+    <div className="lg:col-span-1">
       <div className="h-full py-8 px-6 space-y-6 rounded-xl border border-gray-200 bg-white">
         <div className="flex flex-col">
-          <h5 className="text-xl text-gray-600 text-center">{title}</h5>
+          <h5 className="text-xl text-gray-600 text-center font-bold">{title}</h5>
           <div className="mt-2 flex flex-col justify-center gap-4">
             {children}
           </div>
