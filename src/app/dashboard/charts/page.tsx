0ec43bd51e3a3e335050498484282255e3ab1ba7
Schubert Rodriguez Quezada <schubert.quezada@galp.com>
2024-07-25 22:37:21 +0100

    refactor: Update ChartPage component to fetch user's pending and completed todos

--- src/app/dashboard/charts/page.tsx
+++ src/app/dashboard/charts/page.tsx
@@ -1,11 +1,40 @@
-import { Donut } from "@/charts/Donut";
+import { getUserServerSession } from '@/auth/actions/auth-actions'
+import { Donut } from '@/charts'
+import prisma from '@/lib/prisma'
+import { redirect } from 'next/navigation'
 
-export default function ChartPage() {
+export default async function ChartPage() {
+  const user = await getUserServerSession()
+
+  if (!user) {
+    redirect('/api/auth/signin')
+  }
+  //! ERROR, this invokes fetch from node.js and not from window
+  // const onGetTotalTodos = async () => {
+  //   const total = await apiTodos.getTotalTodos()
+  //   console.log('total', total)
+  //   return { total }
+  // }
+
+  const pending = await prisma.todo.count({
+    where: {
+      userId: user.id,
+      completed: false,
+    },
+  })
+
+  const done = await prisma.todo.count({
+    where: {
+      userId: user.id,
+      completed: true,
+    },
+  })
+
   return (
     <div>
       <h1>Chart Page</h1>
       <div>
-        <Donut />
+        <Donut done={done} pending={pending} />
       </div>
     </div>
   )
