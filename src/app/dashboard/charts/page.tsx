0a15b38a8c2eca63cc793818785211cf3fda0f55
Schubert Rodriguez Quezada <schubert.quezada@galp.com>
2024-07-25 21:40:38 +0100

    feat: Add ChartPage component for displaying charts

---
+++ src/app/dashboard/charts/page.tsx
@@ -0,0 +1,12 @@
+import { Donut } from "@/charts/Donut";
+
+export default function ChartPage() {
+  return (
+    <div>
+      <h1>Chart Page</h1>
+      <div>
+        <Donut />
+      </div>
+    </div>
+  )
+}
